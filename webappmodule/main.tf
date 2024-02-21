198ff1bce65582953c58eaf4dd09ad41e984a146
jayasuryapyenty <160445981+jayasuryapyenty@users.noreply.github.com>
2024-02-22 04:58:35 +0530

    Update main.tf

--- webappmodule/main.tf
+++ webappmodule/main.tf
@@ -3,29 +3,29 @@ provider "azurerm" {
 }
 module "webapp_rg" {
   source = "./resource_group"
-  resource_group_name = "your-webapp-rg"
-  location = "eastus"
+  resource_group_name = var.resource_group_name
+  location = var.location
 }
 
 module "webapp_plan" {
   source = "./appserviceplan"
-  service_plan_name = "your-webapp-plan"
+  service_plan_name = var.service_plan_name
   service_plan_location = module.webapp_rg.resource_group_location
   resource_group_name = module.webapp_rg.resource_group_name
-  sku_size = "B1"
-  sku_tier = "F1"
-}
+  sku_size = var.sku_size
+  sku_tier = var.sku_tier
+  }
 
 module "webapp_insights" {
   source = "./application_insights"
-  insights_name = "your-webapp-insights"
+  insights_name = var.insights_name
   insights_location = module.webapp_rg.resource_group_location
   resource_group_name = module.webapp_rg.resource_group_name
 }
 
 module "webapp" {
   source = "./webapp"
-  webapp_name = "your-webapp"
+  webapp_name = var.webapp_name
   webapp_location = module.webapp_rg.resource_group_location
   resource_group_name = module.webapp_rg.resource_group_name
   app_service_plan_id = module.webapp_plan.app_service_plan_id
